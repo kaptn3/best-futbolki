7197d1ce43efc11dd2b5c16ae9f5b09f32dcd6c0
victoria <kapitonenko.vick@ya.ru>
2019-07-29 22:56:36 +0300

    feat: add structure right nav

--- src/components/RightNav.vue
+++ src/components/RightNav.vue
@@ -1,7 +1,12 @@
 <template>
   <nav>
     <ul>
-      <li>Search</li>
+      <li
+        v-for="item in nav"
+        :key="item.name"
+      >
+        <button :style="{ 'background-image': 'url(' + item.icon + ')' }"/>
+      </li>
     </ul>
   </nav>
 </template>
@@ -9,6 +14,28 @@
 <script>
   export default {
     name: 'RightNav',
+    data() {
+      return {
+        nav: [
+          {
+            name: 'search',
+            icon: '/img/icons/search.svg',
+          },
+          {
+            name: 'wishlist',
+            icon: '/img/icons/like.svg',
+          },
+          {
+            name: 'cart',
+            icon: '/img/icons/cart.svg',
+          },
+          {
+            name: 'profile',
+            icon: '/img/icons/user.svg',
+          },
+        ],
+      };
+    },
   };
 </script>
 
@@ -18,5 +45,30 @@
     align-items: center;
     list-style: none;
     padding: 0;
+  }
+
+  li {
+    display: block;
+    width: 54px;
+    height: 54px;
+    padding: 15px;
+
+    &:hover,
+    &:focus {
+      background-color: #f2f2f2;
+    }
+  }
+
+  button {
+    background-color: transparent;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    border: none;
+    outline: none;
   }
 </style>
