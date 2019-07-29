0803ed803b082139fa92d5d89f7005a360cfab53
victoria <kapitonenko.vick@ya.ru>
2019-07-29 20:08:38 +0300

    chore: vue init

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Product from '@/views/Product.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'product',
+      component: Product,
+    },
+  ],
+});
