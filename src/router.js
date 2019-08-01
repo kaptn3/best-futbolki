ab826272d2bd75be41169312f6b2c33e95d40550
victoria <kapitonenko.vick@ya.ru>
2019-08-01 13:29:03 +0300

    feat: redirect from home to product page

--- src/router.js
+++ src/router.js
@@ -9,6 +9,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/',
+      redirect: '/product',
+    },
+    {
       path: '/product',
       name: 'product',
       component: Product,
