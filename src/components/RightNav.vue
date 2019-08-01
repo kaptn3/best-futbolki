f420e0b64e5e2d591f42a38b4378956ad9143485
victoria <kapitonenko.vick@ya.ru>
2019-08-01 17:32:52 +0300

    feat: slide animation

--- src/components/RightNav.vue
+++ src/components/RightNav.vue
@@ -5,15 +5,31 @@
         v-for="item in nav"
         :key="item.name"
       >
-        <button :style="{ 'background-image': 'url(' + item.icon + ')' }"/>
+        <button
+          :style="{ 'background-image': 'url(' + item.icon + ')' }"
+          @click="open(item.name)"
+        />
+        <transition name="slide-left">
+          <div
+            v-if="$store.state.modalComponent === item.name"
+            class="right-nav__modal"
+          >
+            <cart/>
+          </div>
+        </transition>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+  import Cart from './Cart.vue';
+
   export default {
     name: 'RightNav',
+    components: {
+      Cart,
+    },
     data() {
       return {
         nav: [
@@ -34,7 +50,13 @@
             icon: '/img/icons/user.svg',
           },
         ],
+        modal: '',
       };
+    },
+    methods: {
+      open(box) {
+        this.$store.commit('toggleModal', box);
+      },
     },
   };
 </script>
@@ -70,5 +92,32 @@
     padding: 0;
     border: none;
     outline: none;
+  }
+
+  .right-nav {
+    &__modal {
+      position: fixed;
+      right: 0;
+      top: 0;
+      height: 100vh;
+      overflow-y: auto;
+      width: 100%;
+      max-width: 785px;
+      background-color: #fff;
+      z-index: 100;
+    }
+  }
+
+  .slide-left-enter,
+  .slide-left-leave-to {
+    transform: translateX(100%);
+  }
+
+  .slide-left-enter-active {
+    transition: transform .3s ease;
+  }
+
+  .slide-left-leave-active {
+    transition: transform .3s ease;
   }
 </style>
