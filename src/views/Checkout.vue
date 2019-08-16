b6c7ffe4ad1daca66e7e2c5d9df07eb6d15d17bc
victoria <kapitonenko.vick@ya.ru>
2019-08-16 18:28:53 +0300

    feat: mobile form

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,48 +1,125 @@
 <template>
-  <div class="container checkout">
+  <form
+    ref="form"
+    class="container checkout"
+    @submit="submitForm"
+  >
     <h1>Корзина</h1>
     <h2>Ваши данные</h2>
     <div class="checkout__box">
       <checkout-form/>
       <div class="checkout__right">
-        <cart-item
-          v-for="(item, index) in $store.state.cart"
-          :key="index"
-          :cart="item"
-          :checkout="true"
+        <div class="checkout__items">
+          <cart-item
+            v-for="(item, index) in $store.state.cart"
+            :key="index"
+            :cart="item"
+            :checkout="true"
+          />
+        </div>
+        <a-input
+          id="comment"
+          :required="false"
+          name="comment"
+          placeholder="Комментарий"
         />
       </div>
     </div>
-    <delivery/>
-  </div>
+    <a-button
+      class="checkout__btn"
+      text="Оформить заказ"
+      type="submit"
+    />
+  </form>
 </template>
 
 <script>
+  import axios from 'axios';
   import CheckoutForm from '@/components/CheckoutForm';
+  import AButton from '@/components/Button';
   import CartItem from '@/components/cart/CartItem';
-  import Delivery from '@/components/checkout/Delivery';
+  import AInput from '@/components/Input';
 
   export default {
     name: 'Checkout',
     components: {
       CheckoutForm,
       CartItem,
-      Delivery
+      AButton,
+      AInput
+    },
+    methods: {
+      submitForm(e) {
+        e.preventDefault();
+        const formData = new FormData(this.$refs.form);
+        const data = {};
+        const receiver = {};
+        const address = {
+          'id': 0,
+          'country': 'Россия',
+          'region': '',
+          'region_type': '',
+          'postcode': '0'
+        };
+        for (const [key, val] of formData.entries()) {
+          if (key === 'delivery' || key === 'pickup_point_id' || key === 'payment_alias') {
+            Object.assign(data, { [key]: val });
+          } else if (key === 'city' || key === 'address') {
+            Object.assign(address, { [key]: val });
+          } else {
+            Object.assign(receiver, { [key]: val });
+          }
+        }
+        Object.assign(receiver, {
+          'name': `${receiver['last-name']} ${receiver['first-name']}
+            ${receiver['middle-name']}`
+        });
+        Object.assign(data, { 'receiver': receiver });
+        Object.assign(data, { 'cart': this.$store.state.cart });
+        Object.assign(data, { 'address': address });
+        console.log(data);
+        const url = 'http://api.best-futbolki.ru/API/order.php';
+        axios.post(url, data)
+          .then((res) => {
+            console.log(res);
+          });
+      }
     }
   };
 </script>
 
 <style lang="scss" scoped>
   .checkout {
+    padding-top: 40px;
+    padding-bottom: 40px;
+
     &__box {
       display: flex;
       justify-content: space-between;
+      align-items: flex-start;
     }
 
     &__right {
+      margin-left: 20px;
+    }
+
+    &__items {
       border: 2px solid #bdbdbd;
       border-radius: 8px;
-      padding: 30px 20px;
+      padding: 15px 10px;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .checkout {
+      &__box {
+        display: block;
+      }
+
+      &__right {
+        margin-left: 0;
+        margin-bottom: 20px;
+      }
     }
   }
 </style>
