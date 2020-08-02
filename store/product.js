c1e359b5975b690af661b3f5735a323c719d3643
Victoria <kapitonenko.vick@ya.ru>
2020-08-02 18:57:26 +0300

    feat: add store product

---
+++ store/product.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  product: {},
+  catalogReferences: {}
+});
+
+export const mutations = {
+  setProduct(state, data) {
+    state.product = data;
+  },
+  setCatalogReference(state, data) {
+    state.catalogReferences = data;
+  }
+};
+
+export const actions = {
+  getProduct({ commit }, productId) {
+    return this.$axios
+      .get(`/test_api.php/?i=${productId}`)
+      .then((res) => {
+        console.log(res.data);
+        commit('setProduct', res.data);
+
+        return this.$axios.get('catalog_references.php');
+      })
+      .then((res) => {
+        commit('setCatalogReference', res.data);
+      });
+  }
+};
