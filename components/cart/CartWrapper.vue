302421590635dcd3890c415ee7d0e91c2028c8e2
Victoria <kapitonenko.vick@ya.ru>
2020-08-15 14:55:01 +0300

    feat: add btns

--- components/cart/CartWrapper.vue
+++ components/cart/CartWrapper.vue
@@ -9,6 +9,14 @@
         class="cart__product"
       />
       <CartSummary />
+      <div class="d-flex justify-space-between align-center my-4">
+        <v-btn text>
+          Вернуться к покупкам
+        </v-btn>
+        <v-btn to="/checkout" color="grey darken-3" large dark>
+          Оформить
+        </v-btn>
+      </div>
     </div>
     <div v-else class="cart__box">
       <p class="text-h3 font-weight-medium mb-10">Корзина пуста!</p>
