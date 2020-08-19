3bd80b87c983a9dbff08d5721139f99f86dd0a58
Victoria <kapitonenko.vick@ya.ru>
2020-08-19 08:36:24 +0300

    feat: change position btn

--- components/cart/Cart.vue
+++ components/cart/Cart.vue
@@ -10,6 +10,7 @@
       />
       <CartSummary />
       <div class="d-flex justify-space-between align-center my-4">
+        <v-space />
         <v-btn to="/checkout" color="grey darken-3" large dark>
           Оформить заказ
         </v-btn>
